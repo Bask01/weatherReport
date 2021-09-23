8b70379c201ac5d7588e805ab787b96f8dc583ef
bask01 <bask@sheridancollege.ca>
2021-09-23 17:42:22 -0400

    displays current weather in selected city

---
+++ js/myscript.js
@@ -0,0 +1,55 @@
+var cityArray = new Array();
+var latArray = new Array();
+var lngArray = new Array();
+var rowID;
+
+$(document).ready(function() {
+  console.log("in doc ready");
+
+  $.getJSON("dataFiles/cities.json", function(data) {
+    //console.log(data);
+
+    $("#cittyList").html("");
+    for (let x=0; x < data.cities.length; x++) {
+      cityArray[x] = data.cities[x].cityName;
+      latArray[x] = data.cities[x].cityLat;
+      lngArray[x] = data.cities[x].cityLng;
+
+      //display the cities
+      $("#cityList").append(
+        `
+          <li id='${x}'>
+              <a href='weather.html'>
+                  ${data.cities[x].cityName}
+              </a>
+              <hr>
+          </li>
+        `
+      )
+
+    }//end of for loop
+
+
+
+
+ /* console.log(cityArray);
+    console.log(latArray);
+    console.log(lngArray); */
+
+
+    localStorage.setItem("cityArray", JSON.stringify(cityArray));
+    localStorage.setItem("cityLat", JSON.stringify(latArray));
+    localStorage.setItem("cityLng", JSON.stringify(lngArray));
+
+  });//end of .getJSON
+
+});//end of doc ready
+
+//grab the rowID
+$(document).on("click", "#cityList > li", function() {
+  localStorage.setItem(
+    "rowID",
+    $(this).closest("li").attr("id")
+  
+  );
+})
